58baffffbad3d55519584a94579d9104c0286831
Andrey S. Rudin <rudinandrey@yandex.ru>
2013-05-01 19:36:38 +0400

    all symbols

--- keyboard.js
+++ keyboard.js
@@ -2,12 +2,17 @@ var key2 = new Array("a","b","c","а","б","в","г");
 var key3 = new Array("d","e","f","д","е","ж","з");
 var key4 = new Array("g","h","i","и","й","к","л");
 var key5 = new Array("j","k","l","м","н","о","п");
+var key6 = new Array("m","n","o","р","с","т","у");
+var key7 = new Array("p","q","r", "s" ,"ф","х","ц","ч");
+var key8 = new Array("t","u","v","ш","щ","ъ","ы");
+var key9 = new Array("w","x","y", "z","ь","э","ю","я");
 
-var indexKeys = {"2": 0, "3": 0, "4": 0, "5":0};
+
+var indexKeys = {"2": 0, "3": 0, "4": 0, "5":0, "6":0, "7":0,"8":0, "9":0};
 
 
 var clearIndexes = function () {
-	indexKeys = {"2": 0, "3": 0, "4": 0, "5":0};
+	indexKeys = {"2": 0, "3": 0, "4": 0, "5":0, "6":0, "7":0,"8":0, "9":0};
 };
 
 var increaseIndex = function (arr, key) {
@@ -39,14 +44,29 @@ window.onkeydown = function () {
 			increaseIndex(key3, "3");
 			break;
 		case 52:
-			search.value = search.value + key3[indexKeys["4"]];
+			search.value = search.value + key4[indexKeys["4"]];
 			increaseIndex(key4, "4");
 			break;
-		case 53:
-			search.value = search.value + key3[indexKeys["4"]];
-			increaseIndex(key4, "4");
+			case 53:
+			search.value = search.value + key5[indexKeys["5"]];
+			increaseIndex(key5, "5");
 			break;
-
+			case 54:
+			search.value = search.value + key6[indexKeys["6"]];
+			increaseIndex(key6, "6");
+			break;
+			case 55:
+			search.value = search.value + key7[indexKeys["7"]];
+			increaseIndex(key7, "7");
+			break;
+			case 56:
+			search.value = search.value + key8[indexKeys["8"]];
+			increaseIndex(key8, "8");
+			break;
+			case 57:
+			search.value = search.value + key9[indexKeys["9"]];
+			increaseIndex(key9, "9");
+			break;
 	}
-
+	return false;
 };
