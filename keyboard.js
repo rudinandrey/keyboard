dfdc38fc5b3bc51776ef8f9e8f74b527214ed659
Andrey S. Rudin <rudinandrey@yandex.ru>
2013-05-01 18:30:16 +0400

    Works key 2

--- keyboard.js
+++ keyboard.js
@@ -1,7 +1,26 @@
+var key2 = new Array("a","b","c");
+var indexKeys = {"2": 0, "3": 0, "4": 0, "5":0};
+
+var increaseIndex = function (arr, key) {
+	console.log("arr=" + arr + " ... " + "key=" + key);
+	var length = arr.length;
+	console.log('lenght=' + length);
+	if (indexKeys[key] < length-1) indexKeys[key] += 1;
+	else indexKeys[key] = 0;
+	console.log('index=' + indexKeys[key]);
+}
+
+
 window.onkeydown = function () {
 	var code = window.event.keyCode;
 	var search = document.getElementById("search");
 	var textSearch = search.value;
-	textSearch += code.toString();
-	search.value = textSearch;
+
+	switch (code) {
+		case 50:
+			search.value = search.value + key2[indexKeys["2"]];
+			increaseIndex(key2, "2");
+			break;
+	}
+
 };
