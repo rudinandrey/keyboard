463bb9b60676d2858ee0324e83d6c91ee61388b4
Andrey S. Rudin <rudinandrey@yandex.ru>
2013-05-01 18:49:19 +0400

    clear all indexes
    For correct press other key it needs to clear all array, and save only
    current key index

--- keyboard.js
+++ keyboard.js
@@ -1,14 +1,23 @@
-var key2 = new Array("a","b","c");
+var key2 = new Array("a","b","c","а","б","в","г");
+var key3 = new Array("d","e","f","д","е","ж","з");
 var indexKeys = {"2": 0, "3": 0, "4": 0, "5":0};
+
+
+var clearIndexes = function () {
+	indexKeys = {"2": 0, "3": 0, "4": 0, "5":0};
+};
 
 var increaseIndex = function (arr, key) {
 	console.log("arr=" + arr + " ... " + "key=" + key);
 	var length = arr.length;
+	var currentKey = indexKeys[key];
+	clearIndexes();
 	console.log('lenght=' + length);
-	if (indexKeys[key] < length-1) indexKeys[key] += 1;
+	if (indexKeys[key] < length - 1) indexKeys[key] += 1;
 	else indexKeys[key] = 0;
 	console.log('index=' + indexKeys[key]);
 }
+
 
 
 window.onkeydown = function () {
@@ -20,6 +29,10 @@ window.onkeydown = function () {
 		case 50:
 			search.value = search.value + key2[indexKeys["2"]];
 			increaseIndex(key2, "2");
+			break;
+		case 51:
+			search.value = search.value + key3[indexKeys["3"]];
+			increaseIndex(key3, "3");
 			break;
 	}
 
